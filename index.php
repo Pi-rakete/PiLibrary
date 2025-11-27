935043b9d2b6709b1385e819e4eb6a815adfc1e9
theurrbc <rebecca.theurer@ruhr-uni-bochum.de>
2025-11-27 08:10:31 +0100

    übrige Links auf php korrigiert

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
         <title>Home</title>
     </head>
     <body>
-        <a href="projekt-plan.html">Projekt plan</a>
-        <a href="buecher-uebersicht.html">Bücher Übersicht</a>
-        <a href="dokumentation.html">Dokumentation</a>
+        <a href="projekt-plan.php">Projekt plan</a>
+        <a href="buecher-uebersicht.php">Bücher Übersicht</a>
+        <a href="dokumentation.php">Dokumentation</a>
     </body>
     </html>
